fffcc28c28d8eb6471e27226f57135707521db0a
Niki <nikinatt@icloud.com>
2025-08-21 22:24:45 -0700

    yup

--- src/main/java/frc/robot/commands/composition/Score.java
+++ src/main/java/frc/robot/commands/composition/Score.java
@@ -1,29 +1,43 @@
 package frc.robot.commands.composition;
 
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+import frc.robot.Constants.ScoreSide;
 import frc.robot.RobotContainer;
+import frc.robot.commands.alignment.AlignNearestLeftReef;
+import frc.robot.commands.alignment.AlignNearestRightReef;
 import frc.robot.commands.elevator.RunElevatorCommand;
 import frc.robot.commands.elevator.RunElevatorExplicit;
 import frc.robot.commands.indexer.RunIndexer;
+import frc.robot.commands.indexer.ScoreCoral;
+import frc.robot.commands.indexer.ScoreCoralL4;
 import frc.robot.commands.indexer.StopIndexerCommand;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.elevator.Elevator;
+import frc.robot.subsystems.elevator.Elevator.ElevatorPosition;
 import frc.robot.subsystems.indexer.Indexer;
 
 public class Score extends SequentialCommandGroup {
   public Score(Elevator elevator, Indexer indexer, Drive drive, RobotContainer container) {
     addCommands(
-        elevator.goHome(),
-        new RunElevatorCommand(elevator),
-        new WaitUntilCommand(() -> elevator.getAtDesiredPose()),
-        new RunIndexer(indexer, 2.0, 0.9),
-        new WaitCommand(1.5),
-        new StopIndexerCommand(indexer),
-        new RunElevatorExplicit(elevator, 100),
-        new WaitCommand(0.2),
-        new RunElevatorExplicit(elevator, 0.5),
-        elevator.goHome());
+      elevator.goHome(),
+      new ConditionalCommand(
+          new AlignNearestLeftReef(drive, container),
+          new AlignNearestRightReef(drive, container),
+          () -> side.get() == ScoreSide.LEFT),
+      new RunElevatorCommand(elevator),
+      new WaitUntilCommand(() -> elevator.getAtDesiredPose()),
+      new ConditionalCommand(
+        new ScoreCoral(indexer), 
+        new ScoreCoralL4(indexer), 
+        () -> elevator.getSelectedPosition() == ElevatorPosition.L1 || ElevatorPosition.L2 || Elevator.L3),
+      new WaitCommand(1.5),
+      new StopIndexerCommand(indexer),
+      new RunElevatorExplicit(elevator, 100),
+      new WaitCommand(0.2),
+      new RunElevatorExplicit(elevator, 0.0),
+      elevator.goHome());
   }
 }
